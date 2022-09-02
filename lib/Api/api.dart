df6ef0ca284fc31b9950f256577cd3390c08e820
samuelkanyi <amshelkanyi@gmail.com>
2022-09-02 02:49:15 +0000

    decode Response

--- lib/Api/api.dart
+++ lib/Api/api.dart
@@ -4,14 +4,14 @@ import 'package:anime_quote/model/quote.dart';
 import 'package:http/retry.dart';
 import 'package:http/http.dart' as http;
 
-const String baseUrl = 'https://animechan.vercel.app';
+const String baseUrl = 'animechan.vercel.app';
 
 class Api {
   Future<Quote> fetchRandomQuote() async {
     final client = RetryClient(http.Client());
     try {
-      dynamic result = await client.get(Uri.http(baseUrl, '/api/random'));
-      return Quote.fromJson(jsonDecode(result));
+      http.Response result = await client.get(Uri.http(baseUrl, '/api/random'));
+      return Quote.fromJson(jsonDecode(utf8.decode(result.bodyBytes)));
     } finally {
       client.close();
     }
